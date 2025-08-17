183c0665bdd3c9cfae6de702f2c232af4f75304d
Boluwatife <122864888+Dev-Bolu@users.noreply.github.com>
2025-08-18 00:27:08 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,17 @@
 USE alx_book_store;
 
 SELECT
-    book_id ,
-    title ,
-    author_id ,
-    price DOUBLE ,
-    publication_date  
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NUKKABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
     AND TABLE_NAME = 'Books';
     
+
 
 
 
